011a98c905c155383d55b8ef249f79916ebde517
Greg Barre <g.barre@hotmail.ca>
2022-09-20 17:01:57 -0400

    cleaning up server index.js

--- root/server/index.js
+++ root/server/index.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
 const db = require("./models");
 const cors = require("cors");
 
@@ -38,6 +34,12 @@ db.sequelize.sync().then(() => {
 });
 
 // Socket.io
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
 io.on('connection', socket => {
     console.log(socket.id)
 });
