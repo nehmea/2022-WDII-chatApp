d23d70257d5c79a7b77c84282bae8e898bc649d0
Alina Gotcherian <alina.gotcherian@gmail.com>
2022-09-20 12:54:03 -0400

    added process.env.REACT_APP_SERVER_URL to MessageList

--- root/client/src/components/MessageList.js
+++ root/client/src/components/MessageList.js
@@ -1,36 +1,36 @@
 //import React from 'react';
 import axios from "axios";
-import {useParams} from "react-router-dom";
-import {AuthContext} from '../helpers/AuthContext';
-import {useNavigate} from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { AuthContext } from '../helpers/AuthContext';
+import { useNavigate } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import Message from './Message';
 import React, { useState, useEffect, useContext } from "react";
 
 function MessageList() {
 
-    let {channelId} = useParams();
-    const [listOfMessages, setListOfMessages] = useState([]);
-    const {authState} = useContext(AuthContext);
+  let { channelId } = useParams();
+  const [listOfMessages, setListOfMessages] = useState([]);
+  const { authState } = useContext(AuthContext);
 
-    useEffect(() => {
-        axios.get(`http://localhost:3001/messages/byChannel/${channelId}`).then((response) => {
-            setListOfMessages(response.data);
-        });
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_SERVER_URL}/messages/byChannel/${channelId}`).then((response) => {
+      setListOfMessages(response.data);
+    });
 
-    }, []);
+  }, []);
 
   return (
     <div>
-        {listOfMessages.map((value, key) => {
-            return (
-                <Message key={key} 
-                    username={value.user.username}
-                    createdAt={value.createdAt}
-                    body={value.body}
-                />  
-            );
-        })}     
+      {listOfMessages.map((value, key) => {
+        return (
+          <Message key={key}
+            username={value.user.username}
+            createdAt={value.createdAt}
+            body={value.body}
+          />
+        );
+      })}
     </div>
   )
 }
