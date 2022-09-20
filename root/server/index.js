adeead4655541003a18762fe74245de1c57cdee4
Greg Barre <g.barre@hotmail.ca>
2022-09-20 19:07:15 -0400

    cleanup

--- root/server/index.js
+++ root/server/index.js
@@ -33,13 +33,3 @@ db.sequelize.sync().then(() => {
   });
 });
 
-// Socket.io
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-
-
-io.on('connection', socket => {
-    console.log(socket.id)
-});
