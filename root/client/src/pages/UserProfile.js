ebe71ba6957bf99e2b15c72da279403ae883b5f7
nehmea <ali.hassan.nehme@gmail.com>
2022-09-23 11:29:53 -0400

    removed info in user profile and kept buttons

--- root/client/src/pages/UserProfile.js
+++ root/client/src/pages/UserProfile.js
@@ -8,23 +8,23 @@ import { Link } from "react-router-dom";
 import { useEffect } from "react";
 
 function UserProfile() {
-  const [userInfo, setUserInfo] = useState({});
-  useEffect(() => {
-    console.log("Hello");
-  });
-  getCurrentUserInfo({ setUserInfo });
-  //   console.log(userInfo);
+  //   const [userInfo, setUserInfo] = useState({});
+  //   useEffect(() => {
+  //     console.log("Hello");
+  //   });
+  //   getCurrentUserInfo({ setUserInfo });
+  //   //   console.log(userInfo);
 
-  const createdAt = format(parseISO(userInfo.createdAt), "MMM dd yyyy");
-  const updatedAt = format(parseISO(userInfo.updatedAt), "MMM dd yyyy hh:mmaa");
+  //   const createdAt = format(parseISO(userInfo.createdAt), "MMM dd yyyy");
+  //   const updatedAt = format(parseISO(userInfo.updatedAt), "MMM dd yyyy hh:mmaa");
 
   return (
     <AuthLayout>
-      {console.log(userInfo)}
+      {/* {console.log(userInfo)} */}
       <div className="d-flex align-items-center" style={{ height: "100%" }}>
         <Container className="auth-forms rounded p-4">
           <h2>User Profile</h2>
-          <h4 className="text-light mb-5 mt-5">
+          {/* <h4 className="text-light mb-5 mt-5">
             Username: {userInfo.username}
           </h4>
           <h4 className="text-light">Bio:</h4>
@@ -32,7 +32,7 @@ function UserProfile() {
           <h4 className="text-light">Avatar Url:</h4>
           <p className="fs-6 mb-5">{userInfo.avatarUrl}</p>
           <h4 className="text-light mb-5">Member Since: {createdAt}</h4>
-          <h4 className="text-light">Last Updated: {updatedAt}</h4>
+          <h4 className="text-light">Last Updated: {updatedAt}</h4> */}
 
           <div className="mt-5">
             <Link to="/profile/edit" className="mx-3">
