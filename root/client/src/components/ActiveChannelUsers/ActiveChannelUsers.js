8fced7cef2b0b11401d2b43fa7eceeb8675d509d
Alina Gotcherian <alina.gotcherian@gmail.com>
2022-09-26 16:48:33 -0400

    added user avatar to top right corner

--- root/client/src/components/ActiveChannelUsers/ActiveChannelUsers.js
+++ root/client/src/components/ActiveChannelUsers/ActiveChannelUsers.js
@@ -1,30 +1,36 @@
-import React, { useContext } from "react";
-import { AuthContext } from "../../helpers/AuthContext";
+import React, { useEffect, useState } from "react";
+import { getCurrentUserInfo } from "../../helpers/Utils";
 import "./ActiveChannelUsers.css";
 
 function ActiveChannelUsers({ activeChannelUsers }) {
-  const { authState } = useContext(AuthContext);
+  const [userInfo, setUserInfo] = useState(null);
+  useEffect(() => {
+    getCurrentUserInfo({ setUserInfo });
+  }, []);
+
   return (
     <>
       {/* Header */}
       <div className='home-nav-header channel-users-nav p-2'>
-        {/* <div className="position-relative">
-          <img
-            alt="user avatar"
-            src={user.avatarUrl}
-            width="45"
-            height="45"
-            className="rounded-circle avatar me-3"
-          />
+        {userInfo &&
+          <div className="position-relative">
+            <img
+              alt="user avatar"
+              src={userInfo.avatarUrl}
+              width="45"
+              height="45"
+              className="rounded-circle avatar me-3"
+            />
 
-          {user.status === "online" ? (
-            <div className="rounded-circle position-absolute status-icon online-green"></div>
-          ) : user.status === "busy" ? (
-            <div className="rounded-circle position-absolute status-icon busy-red"></div>
-          ) : (
-            <div className="rounded-circle position-absolute status-icon offline-empty"></div>
-          )}
-        </div>*/}
+            {userInfo.status === "online" ? (
+              <div className="rounded-circle position-absolute status-icon online-green"></div>
+            ) : userInfo.status === "busy" ? (
+              <div className="rounded-circle position-absolute status-icon busy-red"></div>
+            ) : (
+              <div className="rounded-circle position-absolute status-icon offline-empty"></div>
+            )}
+          </div>
+        }
       </div>
       <div className="d-flex flex-column m-2">
         <h2>Member List</h2>
